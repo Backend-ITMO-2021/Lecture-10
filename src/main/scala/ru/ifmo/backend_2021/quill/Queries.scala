b2cf61ae2dc297b0e3e65dd8110029d8a81f44af
Dmitry Puzyrev <hello@dmitry.engineer>
2021-05-26 15:13:13 +0300

    Task 1.a

--- src/main/scala/ru/ifmo/backend_2021/quill/Queries.scala
+++ src/main/scala/ru/ifmo/backend_2021/quill/Queries.scala
@@ -1,8 +1,23 @@
 package ru.ifmo.backend_2021.quill
 
-import io.getquill.{LowerCase, PostgresJdbcContext}
+import io.getquill._
 
 object Queries {
-  def topTenLanguagesSpokenByCities(ctx: PostgresJdbcContext[LowerCase.type]): List[(String, Long)] = ???
+  def topTenLanguagesSpokenByCities(ctx: PostgresJdbcContext[LowerCase.type]): List[(String, Long)] = {
+    import ctx._
+
+    val q = quote {
+      query[CountryLanguage]
+        .join(query[City])
+        .on{(l, c) => l.countryCode == c.countryCode}
+        .groupBy{case (l, c) => l.language}
+        .map{case (language, lc) => (language, lc.size)}
+        .sortBy{case (language, size) => size}(Ord.descNullsLast)
+        .take(10)
+    }
+
+    ctx.run(q)
+  }
+
   def topTenLanguagesSpokenByPopulation(ctx: PostgresJdbcContext[LowerCase.type]): List[(String, Long)] = ???
 }
