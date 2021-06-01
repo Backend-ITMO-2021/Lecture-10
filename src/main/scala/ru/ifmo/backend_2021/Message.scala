093ba14d53f1dae94cf48b839ac8bfc320772ded
ThreshHNS <herovelts1@gmail.com>
2021-06-02 02:05:42 +0300

    Начали выполнять лаб. работу

--- src/main/scala/ru/ifmo/backend_2021/Message.scala
+++ src/main/scala/ru/ifmo/backend_2021/Message.scala
@@ -1,12 +1,8 @@
 package ru.ifmo.backend_2021
 
-case class Message(username: String, message: String) {
-  def toFile: String = s"$username#$message"
-}
-
-object Message {
-  def apply(fromString: String): Message = {
-    val List(username, message) = fromString.split("#").toList
-    Message(username, message)
-  }
-}
+case class Message(
+    id: Int,
+    username: String,
+    message: String,
+    parentId: Option[Int]
+) {}
--- src/main/scala/ru/ifmo/backend_2021/db/DBAdapter.scala
+++ src/main/scala/ru/ifmo/backend_2021/db/DBAdapter.scala
@@ -6,7 +6,8 @@ import io.getquill.{LowerCase, PostgresJdbcContext}
 import ru.ifmo.backend_2021.Message
 
 class DBAdapter() extends MessageDB {
-  val server: EmbeddedPostgres = EmbeddedPostgres.builder()
+  val server: EmbeddedPostgres = EmbeddedPostgres
+    .builder()
     .setDataDirectory("./data")
     .setCleanDataDirectory(false)
     .setPort(5432)
@@ -15,17 +16,23 @@ class DBAdapter() extends MessageDB {
   pgDataSource.setUser("postgres")
   val hikariConfig = new HikariConfig()
   hikariConfig.setDataSource(pgDataSource)
-  val ctx = new PostgresJdbcContext(LowerCase, new HikariDataSource(hikariConfig))
-  ctx.executeAction("CREATE TABLE IF NOT EXISTS message (username text, message text);")
+  val ctx =
+    new PostgresJdbcContext(LowerCase, new HikariDataSource(hikariConfig))
+  ctx.executeAction(
+    "CREATE TABLE IF NOT EXISTS message (id serial, username text, message text, parentId int, timestamp timestamp not null default now());"
+  )
 
   import ctx._
 
   def getMessages: List[Message] =
     ctx.run(query[Message])
+
   def addMessage(message: Message): Unit =
-    ctx.run(query[Message].insert(lift(message)))
+    ctx.run(
+      query[Message].insert(
+        _.username -> lift(message.username),
+        _.message -> lift(message.message),
+        _.parentId -> lift(message.parentId)
+      )
+    )
 }
-
-
-
-
