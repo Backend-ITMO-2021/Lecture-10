6e7eb46434274e3ce761b3dc7913e50f4d6eded5
Panda0412 <nasb_99@mail.ru>
2021-06-06 01:00:43 +0300

    tests

--- src/test/scala/RedditTest.scala
+++ src/test/scala/RedditTest.scala
@@ -17,17 +17,7 @@ object RedditTest extends TestSuite {
       val success = requests.get(host)
 
       assert(success.text().contains("Reddit: Swain is mad :("))
-      assert(success.text().contains("ventus976"))
-      assert(success.text().contains("I don't particularly care which interaction they pick so long as it's consistent."))
-      assert(success.text().contains("XimbalaHu3"))
-      assert(success.text().contains("Exactly, both is fine but do pick one."))
       assert(success.statusCode == 200)
-
-      val wsMsg = Await.result(wsPromise.future, Inf)
-      assert(wsMsg.contains("ventus976"))
-      assert(wsMsg.contains("I don't particularly care which interaction they pick so long as it's consistent."))
-      assert(wsMsg.contains("XimbalaHu3"))
-      assert(wsMsg.contains("Exactly, both is fine but do pick one."))
 
       wsPromise = scala.concurrent.Promise[String]
       val response = requests.post(host, data = ujson.Obj("name" -> "ilya", "msg" -> "Test Message!"))
@@ -37,19 +27,11 @@ object RedditTest extends TestSuite {
       assert(parsed("err") == ujson.Str(""))
 
       assert(response.statusCode == 200)
-      val wsMsg2 = Await.result(wsPromise.future, Inf)
-      assert(wsMsg2.contains("ventus976"))
-      assert(wsMsg2.contains("I don't particularly care which interaction they pick so long as it's consistent."))
-      assert(wsMsg2.contains("XimbalaHu3"))
-      assert(wsMsg2.contains("Exactly, both is fine but do pick one."))
-      assert(wsMsg2.contains("ilya"))
-      assert(wsMsg2.contains("Test Message!"))
+      val wsMsg = Await.result(wsPromise.future, Inf)
+      assert(wsMsg.contains("ilya"))
+      assert(wsMsg.contains("Test Message!"))
 
       val success2 = requests.get(host)
-      assert(success2.text().contains("ventus976"))
-      assert(success2.text().contains("I don't particularly care which interaction they pick so long as it's consistent."))
-      assert(success2.text().contains("XimbalaHu3"))
-      assert(success2.text().contains("Exactly, both is fine but do pick one."))
       assert(success2.text().contains("ilya"))
       assert(success2.text().contains("Test Message!"))
       assert(success2.statusCode == 200)
@@ -72,11 +54,74 @@ object RedditTest extends TestSuite {
         ujson.read(response4) ==
           ujson.Obj("success" -> false, "err" -> "Username cannot contain '#'")
       )
+      val response5 = requests.post(host, data = ujson.Obj("name" -> "ilya", "msg" -> "Test Message!", "replyTo" -> "#2"))
+      assert(
+        ujson.read(response5) ==
+          ujson.Obj("success" -> false, "err" -> "Id to replying must be without '#'")
+      )
+      val response6 = requests.post(host, data = ujson.Obj("name" -> "ilya", "msg" -> "Test Message!", "replyTo" -> "-1"))
+      assert(
+        ujson.read(response6) ==
+          ujson.Obj("success" -> false, "err" -> "Message with given id to replying doesn't exist")
+      )
     }
+    test("task2") - withServer(RedditApplication) { host =>
+      var wsPromise = scala.concurrent.Promise[String]
+      val wsClient = cask.util.WsClient.connect(s"$host/subscribe") {
+        case cask.Ws.Text(msg) => wsPromise.success(msg)
+      }
 
+      val response1 = requests.post(host, data = ujson.Obj("name" -> "ilya", "msg" -> "First Message"))
+      val response2 = requests.post(host, data = ujson.Obj("name" -> "ilya", "msg" -> "Second Message"))
+      val response3 = requests.post(host, data = ujson.Obj("name" -> "ilya", "msg" -> "Third Message"))
+      assert(response1.statusCode == 200)
+      assert(response2.statusCode == 200)
+      assert(response3.statusCode == 200)
+
+      val success = requests.get(host)
+      assert(success.text().contains("#1"))
+      assert(success.text().contains("#2"))
+      assert(success.text().contains("#3"))
+      assert(success.statusCode == 200)
+
+      val response = requests.post(host, data = ujson.Obj("name" -> "ilya", "msg" -> "Reply Message", "replyTo" -> "2"))
+      assert(response.statusCode == 200)
+      val success1 = requests.get(host)
+      assert(success1.text().contains("   #5"))
+      assert(success1.statusCode == 200)
+      assert(success1.text().indexOf("#5") < success1.text().indexOf("#3"))
+      assert(success1.text().indexOf("#5") > success1.text().indexOf("#2"))
+    }
+    test("task3") - withServer(RedditApplication) { host =>
+      var wsPromise = scala.concurrent.Promise[String]
+      val wsClient = cask.util.WsClient.connect(s"$host/subscribe") {
+        case cask.Ws.Text(msg) => wsPromise.success(msg)
+      }
+
+      val response = requests.post(host, data = ujson.Obj("name" -> "panda", "msg" -> "buuu"))
+
+      val allMessages = requests.get(host + "/messages")
+      val allMessagesParsed = ujson.read(allMessages)
+      assert(allMessagesParsed("messages").arr.length == 6)
+
+      val topUsers = requests.get(host + "/stats/top")
+      val topUsersParsed = ujson.read(topUsers)
+      assert(topUsersParsed("top").arr.length == 2)
+      assert(topUsers.text().contains("ilya"))
+      assert(topUsers.text().contains("panda"))
+
+
+      val userMessages = requests.get(host + "/messages/ilya")
+      val userMessagesParsed = ujson.read(userMessages)
+      assert(userMessagesParsed("messages").arr.length == 5)
+
+      val userMessagesCount = requests.get(host + "/messages/ilya/stats")
+      assert(userMessagesCount.text().contains("5"))
+    }
     test("javascript") - withServer(RedditApplication) { host =>
       val response1 = requests.get(host + "/static/app.js")
       assert(response1.text().contains("function submitForm()"))
+      assert(response1.text().contains("function applyFilter()"))
     }
   }
 }
